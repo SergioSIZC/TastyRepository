0a027320d9b606467c97e6eab435ef979ebb2526
SergioSIZC <zsergio360@gmail.com>
2024-10-27 02:00:23 -0300

    Form component functionality

--- src/app/recipe-form/recipe-form.component.ts
+++ src/app/recipe-form/recipe-form.component.ts
@@ -3,9 +3,9 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 interface Ingrediente {
-  nombre  : string;
-  cantidad: number;
-  medida  : string;
+  nombre    : string;
+  cantidad? : number;
+  medida    : string;
 }
 interface Foto {
   url         : string; // URL de la foto
@@ -14,9 +14,9 @@ interface Foto {
 interface Receta {
   titulo        : string;
   ingredientes  : Ingrediente[];
-  porciones     : number;
+  porciones?     : number;
   instrucciones : string;
-  duracion      : number;
+  duracion?      : number;
   fotos         : Foto[];
 }
 @Component({
@@ -27,24 +27,70 @@ interface Receta {
   styleUrl: './recipe-form.component.css'
 })
 export class RecipeFormComponent {
-  ingredientes  : Ingrediente[] = [];
-  nombre        : string = '';
-  cantidad      : number = 0;
-  medida        : string = '';
+  // Datos receta
+  recipeTitle     : string = '';
+  ingredientes    : Ingrediente[] = [];
+  recipePortions? : number ;
+  recipeDuration? : number;
+  recipeSteps     : string = '';
+  urlFoto         : string | ArrayBuffer | null = null; 
+  photos          : Foto[] = [];
+  // Datos Ingredientes
+  nombre          : string = '';
+  cantidad?       : number;
+  medida          : string = '';
+
+
 
   agregarIngrediente(){
-    const ingrediente : Ingrediente= {
-      nombre: this.nombre,
-      cantidad: this.cantidad,
-      medida: this.medida
-    };
+    if (this.nombre && this.cantidad! > 0 && this.medida) {
+      const ingrediente : Ingrediente= {
+        nombre: this.nombre,
+        cantidad: this.cantidad,
+        medida: this.medida
+      };
+      
+      this.ingredientes.push(ingrediente);
+
+      console.log(ingrediente);
+      // Limpiar inputs de Ingredientes
+      this.nombre = '';
+      this.cantidad = undefined;
+      this.medida = '';
+    }else{
+      alert('Debe rellenar todos los campos de Ingredientes')
+    }
     
-    this.ingredientes.push(ingrediente);
-    // Limpiar inputs de Ingredientes
-    this.nombre = '';
-    this.cantidad = 0;
-    this.medida = '';
-    // Limpiar inputs de Receta
-    console.log(ingrediente);
+    
+  }
+  onFileSelected( event :Event){
+    const file = (event.target as HTMLInputElement).files?.[0];
+    if(file){
+      const reader = new FileReader();
+      reader.onload = ()=>{
+        if(reader.result){
+          this.photos.push({url:reader.result.toString()});
+        }
+      };
+      reader.readAsDataURL(file);
+
+      
+    }
+  }
+  agregarReceta(){
+
+    
+
+    const receta : Receta = {
+      titulo        : this.recipeTitle,
+      ingredientes  : this.ingredientes,
+      porciones     : this.recipePortions,
+      instrucciones : this.recipeSteps,
+      duracion      : this.recipeDuration,
+      fotos         : this.photos,
+    };
+
+    localStorage.setItem('recetas', JSON.stringify(receta));
+    console.log(receta);
   }
 }
